7aafe7cbd1378bdf41004d4878d3e512504ab62a
M Pacer <mpacer@berkeley.edu>
2017-10-23 13:55:32 -0700

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Setup module for the jupyterlab-latex
+"""
+import setuptools
+
+setuptools.setup(
+    name='jupyterlab_latex',
+    description='A Jupyter Notebook server extension which acts as an endpoint for LaTeX.',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    author          = 'Jupyter Development Team',
+    author_email    = 'jupyter@googlegroups.com',
+    url             = 'http://jupyter.org',
+    license         = 'BSD',
+    platforms       = "Linux, Mac OS X, Windows",
+    keywords        = ['Jupyter', 'JupyterLab', 'LaTeX'],
+    classifiers     = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'notebook'
+    ],
+    package_data={'jupyterlab_latex':['*']},
+)
