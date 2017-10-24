a4c99dad2d42fe2f4d18ac4bbd04b3e82ac4c957
M Pacer <mpacer@berkeley.edu>
2017-10-23 19:24:46 -0700

    fix " to ' with tslint

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,24 @@ import {
   ILayoutRestorer,
   JupyterLab,
   JupyterLabPlugin
-} from "@jupyterlab/application";
+} from '@jupyterlab/application';
 
-import { IStateDB, PathExt } from "@jupyterlab/coreutils";
+import { IStateDB, PathExt } from '@jupyterlab/coreutils';
 
-import { IDocumentManager } from "@jupyterlab/docmanager";
+import { IDocumentManager } from '@jupyterlab/docmanager';
 
-import { IEditorTracker } from "@jupyterlab/fileeditor";
+import { IEditorTracker } from '@jupyterlab/fileeditor';
 
-import "../style/index.css";
+import '../style/index.css';
 
 namespace CommandIDs {
-  export const openLatexPreview = "latex:open-preview";
+  export const openLatexPreview = 'latex:open-preview';
 }
 /**
  * The JupyterLab plugin for the GitHub Filebrowser.
  */
 const latexPlugin: JupyterLabPlugin<void> = {
-  id: "jupyterlab-latex:open",
+  id: 'jupyterlab-latex:open',
   // IDocumentManager: manages files (opening, closing, &c..)
   // ILayoutRestorer: manages layout on refresh
   // IStateDB: restores state on refresh
@@ -51,29 +51,29 @@ function activateLatexPlugin(
         return;
       }
       let pdfFileName = () => {
-        return PathExt.basename(widget.context.path, ".tex") + ".pdf";
+        return PathExt.basename(widget.context.path, '.tex') + '.pdf';
       };
       widget.context.fileChanged.connect((sender, args) => {
-        console.log("caught the update " + pdfFileName());
+        console.log('caught the update ' + pdfFileName());
         manager.openOrReveal(pdfFileName());
       });
       manager.openOrReveal(pdfFileName());
-      console.log("executed preview");
+      console.log('executed preview');
     },
     isEnabled: hasWidget,
     isVisible: () => {
       let widget = editorTracker.currentWidget;
       return (
-        (widget && PathExt.extname(widget.context.path) === ".tex") || false
+        (widget && PathExt.extname(widget.context.path) === '.tex') || false
       );
     },
-    label: "Show LaTeX Preview"
+    label: 'Show LaTeX Preview'
   });
   app.contextMenu.addItem({
     command: CommandIDs.openLatexPreview,
-    selector: ".jp-FileEditor"
+    selector: '.jp-FileEditor'
   });
-  console.log("Activated!");
+  console.log('Activated!');
   return;
 }
 
