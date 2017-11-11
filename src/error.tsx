594787f0dd9ace6ea87a54135af0ef06a94fc9a6
M Pacer <mpacer@berkeley.edu>
2017-11-10 16:40:55 -0800

    use react for component

---
+++ src/error.tsx
@@ -0,0 +1,48 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const LATEX_ERROR_PANEL = 'jp-LatexErrorPanel';
+const LATEX_ERROR_CONTAINER = 'jp-LatexErrorContainer';
+
+/**
+ * A widget which hosts the error logs from LaTeX
+ * when document compilation fails.
+ */
+export
+class ErrorPanel extends Widget {
+  /**
+   * Construct the error panel.
+   */
+  constructor() {
+    super();
+    this.addClass(LATEX_ERROR_PANEL);
+  }
+
+  set text(value: string) {
+    ReactDOM.render(<LatexError text={this.text}/>, this.node);
+  }
+}
+
+export
+interface ILatexProps extends React.Props<LatexError> {
+  text: string;
+}
+
+export
+class LatexError extends React.Component<ILatexProps, {}> {
+
+  render() {
+    return(
+      <div className={LATEX_ERROR_CONTAINER}>
+        <pre>{this.props.text}</pre>
+      </div>
+    );
+  }
+}
