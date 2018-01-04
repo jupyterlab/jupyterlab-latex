7ec914d94b9118ea4d537b1371edfd0174275771
M Pacer <mpacer@berkeley.edu>
2018-01-04 00:06:19 -0800

    update server to use bibtex if a .bib file is present

--- jupyterlab_latex/__init__.py
+++ jupyterlab_latex/__init__.py
@@ -3,6 +3,7 @@ import json
 import os
 import subprocess
 import glob
+import re
 
 from contextlib import contextmanager
 from subprocess import PIPE
@@ -43,45 +44,74 @@ class LatexConfig(Configurable):
     """
     latex_command = Unicode('xelatex', config=True,
         help='The LaTeX command to use when compiling ".tex" files.')
+    bib_command = Unicode('bibtex', config=True,
+        help='The bibTeX command to use when compiling ".tex" files.')
 
 
 class LatexHandler(APIHandler):
     """
     A handler that runs LaTeX on the server.
     """
+    
+    
+    @gen.coroutine
+    def run_latex(self, tex_dir, tex_base_name, whitelist):
+        c = LatexConfig(config=self.config)
+        
+        with latex_cleanup(
+            workdir=tex_dir,
+            whitelist=whitelist
+            ):
+            full_latex_sequence = [
+                c.latex_command,
+                "-interaction=nonstopmode",
+                "-halt-on-error",
+                "-file-line-error",
+                f"{tex_base_name}",
+                ]
+            full_bibtex_sequence = [
+                c.bib_command,
+                f"{tex_base_name}",
+                ]
+            command_sequence = [list(full_latex_sequence)]
+            
+            if any([re.match(r'.*\.bib', x) for x in set(glob.glob("*"))]):
+                command_sequence += [
+                    list(full_bibtex_sequence), 
+                    list(full_latex_sequence), 
+                    list(full_latex_sequence),
+                    ]
+                    
+            for i, cmd in enumerate(command_sequence):
+                process = Subprocess(cmd, 
+                    stdout=Subprocess.STREAM, stderr=Subprocess.STREAM)
+                try:
+                    yield process.wait_for_exit()
+                except CalledProcessError as err:
+                    self.set_status(500)
+                    self.log.error((f'LaTeX command ' 
+                                     '`{" ".join(command_sequence[i])}` '
+                                     'errored with code: ')
+                                   + str(err.returncode))
+                    out = yield process.stdout.read_until_close()
+                    return out
+                
+            return "LaTeX compiled"
+
+    
     @gen.coroutine
     def get(self, path = ''):
         """
         Given a path, run LaTeX, responding when done.
         """
         # Get access to the notebook config object
-        c = LatexConfig(config=self.config)
-
-        tex_file_path = os.path.abspath(path.strip('/'))
-        tex_dir = os.path.dirname(tex_file_path)
-
-        tex_file_name = os.path.basename(tex_file_path)
-        output_filename = os.path.splitext(tex_file_name)[0]+".pdf"
-        with latex_cleanup(
-            workdir=tex_dir,
-            whitelist=[output_filename]
-            ):
-            process = Subprocess([
-                    c.latex_command,
-                    "-interaction=nonstopmode",
-                    "-halt-on-error",
-                    "-file-line-error",
-                    f"{tex_file_name}",
-                ], stdout=Subprocess.STREAM, stderr=Subprocess.STREAM)
-            try:
-                yield process.wait_for_exit()
-                self.finish("LaTeX compiled")
-            except CalledProcessError as err:
-                self.set_status(500)
-                self.log.error('LaTeX command errored with code: '
-                               + str(err.returncode))
-                out = yield process.stdout.read_until_close()
-                self.finish(out)
+        tex_file_path=os.path.abspath(path.strip('/'))
+        tex_file_name=os.path.basename(tex_file_path)
+        tex_dir=os.path.dirname(tex_file_path)
+        tex_base_name =  os.path.splitext(tex_file_name)[0]
+        whitelist = [tex_base_name+'.pdf']
+        out = yield self.run_latex(tex_dir, tex_base_name, whitelist)
+        self.finish(out)
 
 def _jupyter_server_extension_paths():
     return [{
