0e64f89734a066b778406ac79c5de4d91f2d909c
Konstantin Taletskiy <konstantin@taletskiy.com>
2021-07-19 19:37:26 -0700

    refactor: Point to the specific package.json

--- jupyterlab_latex/_version.py
+++ jupyterlab_latex/_version.py
@@ -4,15 +4,14 @@ from pathlib import Path
 __all__ = ["__version__"]
 
 def _fetchVersion():
-    HERE = Path(__file__).parent.resolve()
+    settings = Path(__file__).parent.resolve() / "labextension/package.json"
 
-    for settings in HERE.rglob("package.json"): 
-        try:
-            with settings.open() as f:
-                return json.load(f)["version"]
-        except FileNotFoundError:
-            pass
+    try:
+        with settings.open() as f:
+            return json.load(f)["version"]
+    except FileNotFoundError:
+        pass
 
-    raise FileNotFoundError(f"Could not find package.json under dir {HERE!s}")
+    raise FileNotFoundError(f"Could not find package.json at {settings!s}")
 
 __version__ = _fetchVersion()
