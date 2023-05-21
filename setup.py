d5fcc6f11142ec5b8155ed01bbda3c764c05b1db
Konstantin Taletskiy <konstantin@taletskiy.com>
2023-05-21 21:15:27 +0200

    bump supported Python version

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup_dict = dict(
     zip_safe=False,
     include_package_data=True,
     keywords= ['Jupyter', 'JupyterLab', 'LaTeX'],
-    python_requires = '>=3.6',
+    python_requires = '>=3.8',
     classifiers     = [
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
@@ -77,10 +77,10 @@ setup_dict = dict(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Framework :: Jupyter',
     ],
     install_requires=[
