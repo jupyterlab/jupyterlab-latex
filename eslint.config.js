4bf1b52ffc95906fa30fb5fc05aed11d1336e37c
Akshat Saini <akshatsaini1@gmail.com>
2024-08-30 14:48:28 -0400

    chore: ran prettier on eslint.config and package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   overrides: [
     {
-      files: ["**/*.ts", "**/*.tsx"],
+      files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: 'tsconfig.json',
-        sourceType: 'module',
+        sourceType: 'module'
       },
       plugins: ['@typescript-eslint'],
       rules: {
         // TypeScript rules here
-      },
+      }
     },
     {
-      files: ["**/*.js"],
+      files: ['**/*.js'],
       parserOptions: {
         // No project option needed for JS files
       },
       rules: {
         // JavaScript rules here
-      },
+      }
     }
   ]
 };
